244e7f41802c554330042db30782790166ce32f1
all.silver__ <nno3onn@naver.com>
2021-04-18 14:23:34 +0900

    update

--- public/index.js
+++ public/index.js
@@ -3,12 +3,12 @@ let socket = io();
 
 $(window).resize(isMobile);
 
-function dropdown(button) {
-    const time = parseInt(button.charAt(0));
-    $(".dropdownMenuButton").html(button);
-    time = isNaN(time) ? 0 : time*1000;
-    socket.emit('dropdown', time);
+function dropdown(time) {
+    let name = (time==0) ? 'stop' : time + ' sec';
+    $(".dropdownMenuButton").html(name);
+    socket.emit('dropdown', time*1000);
 }
+
 function isMobile() {
     $(".catImage").css(isVertical(),
         matchMedia("screen and (max-width:480px)").matches 
@@ -21,7 +21,7 @@ function isVertical() {
     return vertical == true ? 'height': 'width';
 }
 function resizeImage(imgFiles, current){
-    const img = new Image();
+    let img = new Image();
     img.src = './images/' + imgFiles[current];
     const width = img.width;
     const height = img.height;
@@ -35,13 +35,6 @@ function displayInfo(current, total, imgFiles) {
     const img = resizeImage(imgFiles, current);
     $('.catImage').attr("src", img.src);
     $('.pages').text(current+1 + '/' + total);
-}
-
-function dropdown(button) {
-    $(".dropdownMenuButton").html(button);
-
-    const time = parseInt(button.charAt(0));
-    socket.emit('dropdown', isNaN(time) ? 0 : time*1000);
 }
 
 socket.on('image_display', (current, total, imgFiles) => {
--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const fs = require('fs');
+const readline = require('readline');
+const rl = readline.Interface({
+    input: process.stdin,
+    output: process.stdout,
+});
 
 const app = express();
 app.set('port', process.env.PORT || 3000);
@@ -16,32 +21,35 @@ let current = 0;
 let total, imgFiles;
 let timerId = null;
 
-function clickButton(num, s) {  //s=0:auto, s=1:button / num=0:back, num=1:next
-    if(s == 1 || current+1 == total) {   //이전에 수행중인 auto 시간 제거
-        stop();
-        if(s == 0) return false; //auto에서 마지막페이지일 경우 끝냄
-    }
-    if(num==1){
-        if(current+1 != total) {
-            current += 1;
-            io.emit('image_display', current, total, imgFiles);
-        }
-    } else {
-        if(current != 0) {
-            current -= 1;
-            io.emit('image_display', current, total, imgFiles);
-        }
-    }
+function clickButton(num) { // num: 0(auto) 1(back) 2(next)
     console.log(`client clicked ${num ?'next':'back'} button. (${current+1}/${total})`);
+
+    if(num !== 0) { // 타이머가 실행 중이 아닌 경우
+        stop(); // 타이머가 실행 중일 수 있으므로 타이머를 중지시킴
+    }
+    if(num === 1) { // back button
+        if(current === 0) { // 첫 페이지인 경우
+            return console.log('첫 페이지 입니다.');
+        }
+        current -= 1;
+    } else { // 타이머 실행 중이거나 next button 인 경우
+        if (current+1 === total) { // 현재 페이지가 마지막 페이지인 경우
+            stop(); // 타이머 정지
+            return console.log('마지막 페이지입니다.');
+        }
+        current += 1;
+    }
+    io.emit('image_display', current, total, imgFiles);
 }
 
 function setTime(time) {
     console.log(`set auto time ${time/1000} sec`);
-    if(time == 0) {
-        stop();
-    } else {
-        timerId = setInterval(() => clickButton(1, 0), time);
-    }
+    if (timerId !== null) {
+        if (timerId._destroyed === false) { // 타이머 실행 중인 경우
+            return timerId._repeat = time;
+        }
+    } // 타이머가 실행 중이 아닌 경우
+    timerId = setInterval( () => { clickButton(0); }, time);
 }
 
 function stop() {
@@ -71,23 +79,38 @@ function readFile(){
     });
 };
 
+function readLine() {
+    rl.on('line', (line) => {
+        if(line == 'next') {
+            clickButton(1, 1);
+        }else if(line == 'back') {
+            clickButton(0, 1);
+        }else if(line == 'stop') {
+            stop();
+        }else if(line.slice(0,4) == 'auto') {
+            time = Number(line.slice(4, line.length));
+            if(isNaN(time)==false) setTime(time * 1000);
+        }
+    });
+}
+
 readFile();
+// readLine();
 
 const io = require('socket.io')(server); 
 io.on('connection',  (socket) => {
     console.log(`user connected : ${socket.id}`);    
     io.emit('image_display', current, total, imgFiles);
 
+    readLine();
     watchImage();
 
     socket.on('button', (num) => {
         clickButton(num, 1);
     });
-    
     socket.on('dropdown', (time) => {
         setTime(time);
     });
-
     socket.on('disconnect', () => {
         console.log(`user disconnected : ${socket.id}`);
     });
